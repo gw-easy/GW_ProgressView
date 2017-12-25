c7fbd43c4077e2ad86d5fd1d055b94fc5f99f820
gw <guowei@suntray.com>
2017-12-25 15:50:18 +0800

    添加演示视图

--- GWProgressView/ViewController.m
+++ GWProgressView/ViewController.m
@@ -12,6 +12,8 @@
 
 @property (strong, nonatomic)GWProgressView *progressView;
 
+@property (strong, nonatomic)GWProgressView *progressView2;
+
 @property (assign, nonatomic)float proValue;
 
 @property (assign, nonatomic)float allTime;
@@ -25,6 +27,7 @@
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
     [self.view addSubview:self.progressView];
+    [self.view addSubview:self.progressView2];
     self.allTime = 1000;
     self.timer = [NSTimer scheduledTimerWithTimeInterval:0.01 target:self selector:@selector(timerAction) userInfo:nil repeats:YES];
     
@@ -40,7 +43,7 @@
     }
     
     self.progressView.progress = self.proValue;
-    
+    self.progressView2.progress = self.proValue;
 }
 
 - (GWProgressView *)progressView{
@@ -53,7 +56,15 @@
     return _progressView;
 }
 
+- (GWProgressView *)progressView2{
+    if (!_progressView2) {
+        _progressView2 = [[GWProgressView alloc] initWithFrame:CGRectMake(30, 400, self.view.frame.size.width-60, 20)];
+        _progressView2.trackTintColor = [UIColor lightGrayColor];
 
+        _progressView2.progressTintColor = [UIColor greenColor];
+    }
+    return _progressView2;
+}
 
 
 @end
